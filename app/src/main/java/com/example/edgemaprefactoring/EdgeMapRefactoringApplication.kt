e925b764ec7278012d133d0bf7be7f4cc2bb85e8
HanBI24 <alsltjdrhs@naver.com>
2023-04-05 21:12:52 +0900

    Add hilt and set Naver Map SDK API key to Application class

--- app/src/main/java/com/example/edgemaprefactoring/EdgeMapRefactoringApplication.kt
+++ app/src/main/java/com/example/edgemaprefactoring/EdgeMapRefactoringApplication.kt
@@ -2,11 +2,13 @@ package com.example.edgemaprefactoring
 
 import android.app.Application
 import com.naver.maps.map.NaverMapSdk
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class EdgeMapRefactoringApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-//        NaverMapSdk.getInstance(this).client =
-//            NaverMapSdk.NaverCloudPlatformClient(BuildConfig.ClientID)
+        NaverMapSdk.getInstance(this).client =
+            NaverMapSdk.NaverCloudPlatformClient(BuildConfig.Client_ID)
     }
 }
--- app/src/main/java/com/example/edgemaprefactoring/MainActivity.kt
+++ app/src/main/java/com/example/edgemaprefactoring/MainActivity.kt
@@ -8,9 +8,11 @@ import androidx.compose.ui.Modifier
 import com.example.edgemaprefactoring.ui.theme.EdgeMapRefactoringTheme
 import com.example.feature_main.MainScreen
 import com.naver.maps.map.compose.ExperimentalNaverMapApi
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.MainScope
 
 @ExperimentalNaverMapApi
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
