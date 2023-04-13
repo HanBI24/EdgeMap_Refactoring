050125d6a652ce7542f099f9d18272622e2f45ad
HanBI24 <alsltjdrhs@naver.com>
2023-04-14 01:42:33 +0900

    Add bottom view button

--- feature_main/src/main/java/com/example/feature_main/MainScreen.kt
+++ feature_main/src/main/java/com/example/feature_main/MainScreen.kt
@@ -1,18 +1,20 @@
 package com.example.feature_main
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavDestination
-import androidx.navigation.NavDestination.Companion.hierarchy
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
 import com.example.common.location.rememberFusedLocationSource
 import com.naver.maps.map.compose.*
 
@@ -20,7 +22,7 @@ import com.naver.maps.map.compose.*
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
     Box(
-        modifier = modifier.fillMaxSize()
+        modifier = modifier
     ) {
         val cameraPositionState = rememberCameraPositionState()
         NaverMap(
@@ -34,5 +36,67 @@ fun MainScreen(modifier: Modifier = Modifier) {
                 isLocationButtonEnabled = true,
             ),
         )
+
+        BottomButtonView(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter)
+                .padding(20.dp)
+                .clip(shape = RoundedCornerShape(12.dp))
+        )
+
+    }
+}
+
+@Composable
+fun BottomButtonView(modifier: Modifier = Modifier) {
+    val bottomButtonTitleList = mapOf(
+        "목적지 검색" to Icons.Default.Search,
+        "즐겨찾기" to Icons.Default.Favorite
+    )
+
+    Box(
+        modifier = modifier
+    ) {
+        Row(
+            modifier = Modifier.background(color = Color.White)
+        ) {
+            bottomButtonTitleList.forEach { entry ->
+                BottomButton(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(0.5f),
+                    entry
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun BottomButton(
+    modifier: Modifier = Modifier,
+    bottomButtonEntry: Map.Entry<String, ImageVector>
+) {
+    OutlinedButton(
+        onClick = {},
+        modifier = modifier,
+        border = BorderStroke(0.dp, Color.Transparent),
+        colors = ButtonDefaults
+            .outlinedButtonColors(
+                backgroundColor = Color.White,
+                contentColor = Color.Magenta
+            )
+    ) {
+        Column(
+            verticalArrangement = Arrangement.SpaceAround,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                imageVector = bottomButtonEntry.value,
+                contentDescription = "Bottom Button Icon"
+            )
+            Text(text = bottomButtonEntry.key)
+        }
     }
 }
