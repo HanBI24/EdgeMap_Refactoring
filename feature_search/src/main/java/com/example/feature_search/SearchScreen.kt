59f0ff0be3985a72b9a479ae7462f59201a8ee20
HanBI24 <alsltjdrhs@naver.com>
2023-04-19 23:22:44 +0900

    Add Polygon class for Hexagon column item

--- feature_search/src/main/java/com/example/feature_search/SearchScreen.kt
+++ feature_search/src/main/java/com/example/feature_search/SearchScreen.kt
@@ -1,25 +1,68 @@
 package com.example.feature_search
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+import kotlin.math.cos
+import kotlin.math.sin
 
 @Composable
 fun SearchScreen() {
     Box(
         modifier = Modifier
-            .fillMaxSize(),
-        contentAlignment = Alignment.Center
+            .fillMaxWidth(0.55f)
+            .height(200.dp)
+            .clip(Polygon(6))
+            .background(Color.Yellow)
     ) {
-        Text(
-            text = "Search Screen",
-            fontSize = MaterialTheme.typography.h3.fontSize,
-            fontWeight = FontWeight.Bold
-        )
+        Text("Hello")
+    }
+}
+
+class Polygon(
+    private val sides: Int,
+    private val rotation: Float = 0f
+) : Shape {
+    override fun createOutline(
+        size: Size,
+        layoutDirection: LayoutDirection,
+        density: Density
+    ): Outline {
+        return Outline.Generic(
+            Path().apply {
+                val radius = if (size.width > size.height) size.width / 2f else size.height / 2f
+                val angle = 2.0 * Math.PI / sides
+                val cx = size.width / 2f
+                val cy = size.height / 2f
+                val r = rotation * (Math.PI / 180)
+                moveTo(
+                    cx + (radius * cos(0.0 + r).toFloat()),
+                    cy + (radius * sin(0.0 + r).toFloat())
+                )
+                for (i in 1 until sides) {
+                    lineTo(
+                        cx + (radius * cos(angle * i + r).toFloat()),
+                        cy + (radius * sin(angle * i + r).toFloat())
+                    )
+                }
+                close()
+            })
     }
 }
