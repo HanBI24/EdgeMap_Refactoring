2751f0cf1eb204950ec80abb2bff32e6f5183f24
HanBI24 <alsltjdrhs@naver.com>
2023-04-25 21:49:19 +0900

    Add TourInfoItem for domain module and create Mapper

---
+++ feature_search/data/src/main/java/com/example/data/mapper/Mapper.kt
@@ -0,0 +1,16 @@
+package com.example.data.mapper
+
+import com.example.data.remote.dto.Item
+import com.example.domain.model.TourInfoItem
+
+object Mapper {
+    fun Item.toTourInfoItem(): TourInfoItem {
+        return TourInfoItem(
+            galTitle = galTitle,
+            galWebImageUrl = galWebImageUrl,
+            galPhotographyMonth = galPhotographyMonth,
+            galPhotographyLocation = galPhotographyLocation,
+            galPhotographer = galPhotographer
+        )
+    }
+}
---
+++ feature_search/domain/src/main/java/com/example/domain/model/TourInfoItem.kt
@@ -0,0 +1,9 @@
+package com.example.domain.model
+
+data class TourInfoItem(
+    val galTitle: String,
+    val galWebImageUrl: String,
+    val galPhotographyMonth: String,
+    val galPhotographyLocation: String,
+    val galPhotographer: String
+)
