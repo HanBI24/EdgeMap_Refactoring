9a8c117ed6f0376fe3e6783caea7d0593dda3a8b
HanBI24 <alsltjdrhs@naver.com>
2023-05-08 17:37:23 +0900

    Add to TextField in feature_search module

--- feature_search/src/main/java/com/example/feature_search/SearchScreen.kt
+++ feature_search/src/main/java/com/example/feature_search/SearchScreen.kt
@@ -1,7 +1,12 @@
 package com.example.feature_search
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.example.presentation.viewmodel.SearchScreenViewModel
@@ -11,5 +16,22 @@ import com.example.presentation.viewmodel.SearchScreenViewModel
 fun SearchScreen() {
     val searchScreenViewModel = hiltViewModel<SearchScreenViewModel>()
     val tourInfoItem = searchScreenViewModel.tourInfoState.value.toruInfoItem.collectAsLazyPagingItems()
-    SearchTourList(tourInfoItem)
+    var searchWord by remember { mutableStateOf("") }
+
+    Column(modifier = Modifier.padding(12.dp)) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.CenterHorizontally)
+        ) {
+            Row(modifier = Modifier.fillMaxWidth()) {
+                TextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = searchWord,
+                    onValueChange = { searchWord = it }
+                )
+            }
+        }
+        SearchTourList(tourInfoItem)
+    }
 }
