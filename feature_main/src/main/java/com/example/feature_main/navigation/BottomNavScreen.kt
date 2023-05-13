6404971dacf8931ffd3ff7301776753b9aa891c1
HanBI24 <alsltjdrhs@naver.com>
2023-05-13 15:58:48 +0900

    Add to SearchResult nav path

--- feature_main/src/main/java/com/example/feature_main/navigation/BottomNavScreen.kt
+++ feature_main/src/main/java/com/example/feature_main/navigation/BottomNavScreen.kt
@@ -2,6 +2,7 @@ package com.example.feature_main.navigation
 
 sealed class BottomNavScreen(val route: String){
     object Home: BottomNavScreen(route = "home")
-    object Search : BottomNavScreen(route = "search")
-    object Favorite : BottomNavScreen(route = "favorite")
+    object Search: BottomNavScreen(route = "search")
+    object Favorite: BottomNavScreen(route = "favorite")
+    object SearchResult: BottomNavScreen(route = "search_result")
 }
